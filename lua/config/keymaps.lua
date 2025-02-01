707ea76df7cd991f03c71b5adf7d67f48b2c18b5
Massimo Pavoni <maspavoni@gmail.com>
2025-02-01 16:11:24 +0100

    Better keymaps and no auto formatting + lock

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- formatting
+map({ "n", "v" }, "<c-s-i>", function() LazyVim.format({ force = true }) end, { desc = "Format" })
+
+-- floating terminal
+map("n", "<c-s-/>", function() Snacks.terminal() end, { desc = "Terminal (cwd)" })
